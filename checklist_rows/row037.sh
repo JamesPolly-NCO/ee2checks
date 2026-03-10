c28c3fc2dc235fcbeccfa523defe699da1450d02
James Polly <james.polly@noaa.gov>
2026-03-10 20:16:10 +0000

    updates

--- checklist_rows/row037.sh
+++ checklist_rows/row037.sh
@@ -27,10 +27,36 @@ ushdir=${tmpdir}/ush
 sorcdir=${tmpdir}/sorc
 fixdir=${tmpdir}/fix
 
-find $1/. -type f \( -name "*.sh" -o -name "*.py" -o -name "*.pl" \) > ${logfile}
-find ${jobsdir} -type f >> ${logfile}
+#find $1/. -type f \( -name "*.sh" -o -name "*.py" -o -name "*.pl" \) > ${logfile}
+#find ${jobsdir} -type f >> ${logfile}
+
+find $jobsdir $scriptsdir $ushdir -type f > $logfile
+
+#check to see if script in above listing is:
+#a) called by one of the other scripts in the listing
+#b) not called at all
+while read entry; do
+    tmpbase=$(basename $entry)
+    tmpcallflag=0
+    while read line; do
+        #grep: ignore binary files -I; include file names in output -H
+        grep -IH $tmpbase $line >> ${logfile}.callrefs
+        [[ $? -eq 0 ]] && tmpcallflag=1 
+    done < $logfile
+    if [[ "$tmpcallflag" -eq 0 ]]; then
+        echo $entry >> ${logfile}.notcalled #manually filter to determine what may be non-ops code
+    else
+        echo $entry >> ${logfile}.called
+    fi
+done < $logfile
 
 while read entry; do
     tmpinterp=$(head -n1 $entry)
-    [[ $? -eq 0 ]] && echo $entry $tmpinterp >> ${logfile}.1 || echo NO_HEAD $entry >> ${logfile}.1
-done < ${logfile}
+    if [[ $? -eq 0 ]]; then
+        echo $entry $tmpinterp >> ${logfile}.called.shebangs #manually check shebangs exist
+                                                             #no shebang needed if sourced
+                                                             #check usage in logfile.callrefs
+    else
+        echo $entry >> ${logfile}.called.noshebang
+    fi
+done < $logfile.called
